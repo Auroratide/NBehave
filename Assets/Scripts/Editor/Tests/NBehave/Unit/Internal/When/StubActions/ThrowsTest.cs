04f27b0b3cdb627d7da8bc0cfa6807d7c0fc17fd
auroratide <tf.auroratide@gmail.com>
2017-02-27 07:49:35 -0600

    Throws test written

---
+++ Assets/Scripts/Editor/Tests/NBehave/Unit/Internal/When/StubActions/ThrowsTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+namespace Auroratide.NBehave.Unit.When {
+    using Internal;
+
+    public class ThrowsTest {
+
+        [ExpectedException(typeof(System.Exception))]
+        [Test] public void ShouldThrowGivenException() {
+            new Throws(new System.Exception()).Return();
+        }
+
+        [ExpectedException(typeof(CustomException))]
+        [Test] public void ShouldThrowCustomException() {
+            new Throws(new CustomException()).Return();
+        }
+
+        private class CustomException : System.Exception {}
+
+    }
+
+}
