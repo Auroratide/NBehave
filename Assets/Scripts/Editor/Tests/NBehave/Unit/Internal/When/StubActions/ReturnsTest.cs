dcafee1af37749772b79fddd354e8b44820fa0b9
auroratide <tf.auroratide@gmail.com>
2017-02-27 07:46:30 -0600

    Returns tests written

---
+++ Assets/Scripts/Editor/Tests/NBehave/Unit/Internal/When/StubActions/ReturnsTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+namespace Auroratide.NBehave.Unit.When {
+    using Internal;
+
+    public class ReturnsTest {
+
+        [Test] public void ShouldReturnSameValue() {
+            Assert.That(new Returns(2).Return(), Is.EqualTo(2));
+            Assert.That(new Returns("hello").Return(), Is.EqualTo("hello"));
+        }
+
+    }
+
+}
