67507f65e78c17b5286a2a1f1efac9511a83e026
auroratide <tf.auroratide@gmail.com>
2017-02-27 21:24:27 -0600

    MockedType tests written

---
+++ Assets/Scripts/Editor/Tests/NBehave/Unit/Internal/Mock/MockedTypeTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+namespace Auroratide.NBehave.Unit.MockTest {
+    using Internal;
+
+    public class MockedTypeTest {
+
+        MockedType<Winter> winter;
+        MockedType<Autumn> autumn;
+
+        [SetUp] public void Init() {
+            winter = new MockedType<Winter>(typeof(Winter));
+            autumn = new MockedType<Autumn>(typeof(Autumn));
+        }
+
+        [Test] public void ShouldReturnTypeThatIsBeingMocked() {
+            Assert.That(winter.Type, Is.EqualTo(typeof(Winter)));
+            Assert.That(autumn.Type, Is.EqualTo(typeof(Autumn)));
+        }
+
+        [Test] public void ShouldCreateObjectOfCorrectType() {
+            Assert.That(winter.Create().GetType(), Is.EqualTo(typeof(Winter)));
+            Assert.That(autumn.Create().GetType(), Is.EqualTo(typeof(Autumn)));
+        }
+
+        private class Winter {}
+        private class Autumn {}
+
+    }
+}
